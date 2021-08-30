7558dc611345710916953a8f1ceb367ef59e3ddf
SrenBlikdal <62331372+SrenBlikdal@users.noreply.github.com>
2021-08-30 16:51:02 +0200

    Update 3.0_methylKit.R

--- 3.0_methylKit.R
+++ 3.0_methylKit.R
@@ -30,7 +30,7 @@ file.list<-list(
 "D19.pair1.truncated.gz_bismark_bt2_pe.multiple.deduplicated.bismark.cov.gz",
 "D20.pair1.truncated.gz_bismark_bt2_pe.multiple.deduplicated.bismark.cov.gz") 
 
-my_obj<-methRead(file.list, sample.id = list("D1","D2","D3","D4","D5","D6","D8","D9","D10", "D11","D12","D13","D14","D15","D16","D17","D18","D19","D20"), assembly = "ICSASG_v2", pipeline = "bismarkCoverage", treatment= c(1,1,1,0,1,0,1,1,0,1,0,1,0,0,1,0,0,1,0), context= "CpG", mincov=5)
+my_obj<-methRead(file.list, sample.id = list("D1","D2","D3","D4","D5","D6","D8","D9","D10", "D11","D12","D13","D14","D15","D16","D17","D18","D19","D20"), assembly = "ICSASG_v2", pipeline = "bismarkCoverage", treatment= c(1,1,1,0,1,0,1,1,0,1,0,1,0,0,1,0,0,1,0), context= "CpG", mincov=1)
 #treatment is meta data from Davides records:0 is healthy/Mycoplasma, 1 correspond to sick/Avivibro. 
 #context =CpG, means we only look at CpG sites 
 
